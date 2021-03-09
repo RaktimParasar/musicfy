b770e40fd1396e0ab9f959895928a1b4f472e077
RaktimParasar <raktimparasar96@gmail.com>
2021-03-09 13:28:47 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import Login from "./components/Login";
-import Home from "./components/Home";
 import Generate from "./components/Generate";
+import Home from "./components/Home";
+
+const checkAuth = () => {
+  const token = localStorage.getItem("access_token");
+  console.log("access: " + token);
+  if (!token) {
+    console.log("No token");
+    return false;
+  }
+};
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      checkAuth() ? <Component {...props} /> : <Redirect to={"/"} />
+    }
+  />
+);
 
 const App = () => {
   return (
@@ -10,7 +33,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/redirect" component={Generate} />
-        <Route exact path="/home" component={Home} />
+        <AuthRoute exact path="/home" component={Home} />
       </Switch>
     </Router>
   );
